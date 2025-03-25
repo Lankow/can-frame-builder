ed87e323788df5d0505fe5a0b7240916f3009cf6
Lankow <maciej.lankow@gmail.com>
2025-03-25 22:21:04 +0100

    Add view model for main window.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CanFrameBuilder.Model;
 using CanFrameBuilder.View;
+using CanFrameBuilder.ViewModel;
 using Microsoft.Win32;
 using System;
 using System.Collections.ObjectModel;
@@ -10,62 +11,16 @@ using System.Windows;
 
 namespace CanFrameBuilder
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public ObservableCollection<CANFrame> Entries { get; set; }
-
-        public string OutputDirectory
-        {
-            get => _outputDirectory;
-            set
-            {
-                if (_outputDirectory != value)
-                {
-                    _outputDirectory = value;
-                    OnPropertyChanged(nameof(OutputDirectory));
-                }
-            }
-        }
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private string _outputDirectory;
-        private string _configDirectory;
+        public ObservableCollection<CANFrame> Frames => (DataContext as MainWindowViewModel)?.Frames ?? [];
+        public string OutputDirectory => (DataContext as MainWindowViewModel)?.OutputDirectory ?? Directory.GetCurrentDirectory();
+        public string ConfigDirectory => (DataContext as MainWindowViewModel)?.ConfigDirectory ?? Directory.GetCurrentDirectory();
 
         public MainWindow()
         {
-            DataContext = this;
-            Entries = [];
-            _outputDirectory = Directory.GetCurrentDirectory();
-            _configDirectory = Directory.GetCurrentDirectory();
-
             InitializeComponent();
-        }
-
-        private void BtnLoadConfig_OnClick(object sender, RoutedEventArgs e)
-        {
-            var fileDialog = new OpenFileDialog
-            {
-                Filter = "JSON Config Files | *.json",
-                InitialDirectory = _configDirectory,
-                Title = "Pick CAN Frames JSON config file"
-            };
-
-            var sucess = fileDialog.ShowDialog();
-
-            if (sucess != true) return;
-
-            _configDirectory = fileDialog.FileName;
-            var canFrames = ConfigHandler.LoadConfig(_configDirectory);
-
-            if (canFrames is null || canFrames.Count <= 0) return;
-            Entries.Clear();
-
-            foreach(var canFrame in canFrames) Entries.Add(canFrame);
+            DataContext = new MainWindowViewModel();
         }
 
         private void BtnSaveConfig_OnClick(object sender, RoutedEventArgs e)
@@ -82,98 +37,15 @@ namespace CanFrameBuilder
 
             var configFilePath = saveFileDialog.FileName;
 
-            ConfigHandler.SaveConfig(configFilePath, [.. Entries]);
-        }
-
-        private void BtnPickOutputPath_OnClick(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFolderDialog
-            {
-                InitialDirectory = _outputDirectory
-            };
-
-            var result = dialog.ShowDialog();
-
-            if (result != true) return;
-
-            OutputDirectory = dialog.FolderName;
-        }
-
-        private void BtnAddFrame_OnClick(object sender, RoutedEventArgs e)
-        {
-            var canFrameModal = new CANFrameModal();
-            canFrameModal.ShowDialog();
-
-            if(!canFrameModal.Success) return;
-
-            var frameName = canFrameModal.FrameName;
-            var frameId = canFrameModal.FrameId;
-            var frameDlc = canFrameModal.FrameDlc;
-            var frameChannel = canFrameModal.FrameChannel;
-            var signals = canFrameModal.Signals;
-
-            Entries.Add(new CANFrame(frameName, frameId, frameDlc, frameChannel, [.. signals]));
-        }
-
-        private void BtnEditFrame_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (FrameEntries.SelectedItems.Count == 0) return;
-
-            var selectedItem = FrameEntries.SelectedItems.Cast<CANFrame>().FirstOrDefault();
-            var canFrameModal = new CANFrameModal(selectedItem);
-            canFrameModal.ShowDialog();
-
-            if (!canFrameModal.Success) return;
-
-            var frameName = canFrameModal.FrameName;
-            var frameId = canFrameModal.FrameId;
-            var frameDlc = canFrameModal.FrameDlc;
-            var frameChannel = canFrameModal.FrameChannel;
-            var signals = canFrameModal.Signals;
-
-            int selectedItemIndex = Entries.IndexOf(selectedItem);
-            Entries[selectedItemIndex] = new CANFrame(frameName, frameId, frameDlc, frameChannel, [.. signals]);
-        }
-
-        private void BtnDeleteFrame_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (FrameEntries.SelectedItems.Count == 0) return;
-
-            var selectedItems = FrameEntries.SelectedItems.Cast<CANFrame>().ToList();
-            var result = MessageBox.Show($"Are You sure You want to delete {selectedItems.Count} item(s)?",
-                "Delete CAN Frames", MessageBoxButton.YesNo);
-
-            if (result == MessageBoxResult.Yes)
-            {
-                foreach(var selectedItem in selectedItems)
-                {
-                    Entries.Remove(selectedItem);
-                }
-            }
-        }
-
-        private void BtnClearFrames_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (Entries.Count > 0)
-            {
-                var result = MessageBox.Show("Are You sure You want to clear CAN Frames list?", 
-                    "Clear Messages", MessageBoxButton.YesNo);
-                
-                if (result == MessageBoxResult.Yes)
-                {
-                    Entries.Clear();
-                }
-            }
+            ConfigHandler.SaveConfig(configFilePath, [.. Frames]);
         }
 
         private void BtnGenerate_OnClick(object sender, RoutedEventArgs e)
         {
-            if (!Directory.Exists(_outputDirectory) || _outputDirectory.Equals(string.Empty))
+            if (Directory.Exists(OutputDirectory) && !OutputDirectory.Equals(string.Empty))
             {
-                _outputDirectory = Directory.GetCurrentDirectory();
+                CANFrameGenerator.GenerateClasses([.. Frames], OutputDirectory);
             }
-
-            CANFrameGenerator.GenerateClasses([.. Entries], _outputDirectory);
         }
     }
 }
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,125 @@
+using CanFrameBuilder.Model;
+using CanFrameBuilder.MVVM;
+using CanFrameBuilder.View;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+namespace CanFrameBuilder.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<CANFrame> Frames { get; set; } = [];
+        public string ConfigDirectory { get; set; } = Directory.GetCurrentDirectory();
+        public string OutputDirectory { get; set; } = Directory.GetCurrentDirectory();
+
+        private CANFrame? _selectedItem = null;
+        public CANFrame? SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand AddCommand => new(execute => AddFrame());
+        public RelayCommand EditCommand => new(execute => EditFrame(), canExecute => _selectedItem != null);
+        public RelayCommand DeleteCommand => new(execute => DeleteFrame(), canExecute => _selectedItem != null);
+        public RelayCommand ClearCommand => new(execute => ClearFrames(), canExecute => Frames.Count > 0);
+        public RelayCommand LoadCommand => new(execute => LoadConfig());
+        public RelayCommand OutputCommand => new(execute => PickOutput());
+
+        private void AddFrame()
+        {
+            var canFrameModal = new CANFrameModal();
+            canFrameModal.ShowDialog();
+
+            if (!canFrameModal.Success) return;
+
+            var frameToAdd = ExtractModalFrame(canFrameModal);
+            Frames.Add(frameToAdd);
+        }
+
+        private void EditFrame()
+        {
+            var canFrameModal = new CANFrameModal(_selectedItem);
+            canFrameModal.ShowDialog();
+
+            if (!canFrameModal.Success) return;
+
+            var frameToEdit= ExtractModalFrame(canFrameModal);
+            int selectedItemIndex = Frames.IndexOf(_selectedItem);
+            Frames[selectedItemIndex] = frameToEdit;
+        }
+
+        private void DeleteFrame()
+        {
+            var result = MessageBox.Show($"Are You sure You want to delete this item?",
+                "Delete CAN Frame", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+                Frames.Remove(_selectedItem);
+        }
+
+        private void ClearFrames()
+        {
+            var result = MessageBox.Show("Are You sure You want to clear CAN Frames list?",
+                "Clear Messages", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Frames.Clear();
+            }
+        }
+
+        private void LoadConfig()
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                Filter = "JSON Config Files | *.json",
+                InitialDirectory = ConfigDirectory,
+                Title = "Pick CAN Frames JSON config file"
+            };
+
+            var sucess = fileDialog.ShowDialog();
+
+            if (sucess != true) return;
+
+            ConfigDirectory = fileDialog.FileName;
+            var canFrames = ConfigHandler.LoadConfig(ConfigDirectory);
+
+            if (canFrames is null || canFrames.Count <= 0) return;
+            Frames.Clear();
+
+            foreach (var canFrame in canFrames) Frames.Add(canFrame);
+        }
+
+        private void PickOutput()
+        {
+            var dialog = new OpenFolderDialog
+            {
+                InitialDirectory = OutputDirectory
+            };
+
+            var result = dialog.ShowDialog();
+
+            if (result != true) return;
+
+            OutputDirectory = dialog.FolderName;
+        }
+
+        private CANFrame ExtractModalFrame(CANFrameModal canFrameModal)
+        {
+            var frameName = canFrameModal.FrameName;
+            var frameId = canFrameModal.FrameId;
+            var frameDlc = canFrameModal.FrameDlc;
+            var frameChannel = canFrameModal.FrameChannel;
+            var signals = canFrameModal.Signals;
+
+            return new CANFrame(frameName, frameId, frameDlc, frameChannel, [.. signals]);
+        }
+    }
+}
